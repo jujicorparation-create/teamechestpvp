24e1939663ac2904145419771c4c605075633c70
jujicorparation-create <jujicorparation@gmail.com>
2026-07-03 15:20:58 +0500

    Update AutoEchestPvpMod.java

--- src/main/java/uz/foldmc/echestpvp/AutoEchestPvpMod.java
+++ src/main/java/uz/foldmc/echestpvp/AutoEchestPvpMod.java
@@ -2,15 +2,26 @@ package uz.foldmc.echestpvp;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class AutoEchestPvpMod implements ClientModInitializer {
 
     public static final String MOD_ID = "auto-echest-pvp";
+    public static KeyBinding triggerKey;
 
     @Override
     public void onInitializeClient() {
-        ClientTickEvents.END_CLIENT_TICK.register(EchestTrigger::onTick);
-        System.out.println("[AutoEchestPvP] yuklandi. HP threshold: " + EchestTrigger.HP_THRESHOLD);
-    }
-}
+        triggerKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.auto-echest-pvp.trigger",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_G,
+                "key.categories.auto-echest-pvp"
+        ));
 
+        ClientTickEvents.END_CLIENT_TICK.register(EchestTrigger::onTick);
+        System.out.println("[AutoEchestPvP] yuklandi. HP threshold: " + EchestTrigger.HP_THRESHOLD + ", keybind: G");
+    }
+            }
